9a6fcbbd1f20da18dca0a566608abf6ec976622d
Lusingalisa <lusingalisa7@gmail.com>
2025-02-19 11:17:05 +0300

    initial change

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+export default function Home(){
+
+  let first_number=923;
+  let second_number=7;
+  let sum = first_number+second_number
+
+  let username="Howard King"
+  return( 
+  <div>
+    <h1>DATA SCIENCE FOR AFRICA</h1>
+    <h2>An initiative for 2025</h2>
+    <p>Welcome to our website</p>
+    <strong>{10+10*1000}</strong>
+    <br/>
+    <em>{sum}</em>
+    <p>Greetings {username} </p>
+  </div>
+  )
+}
