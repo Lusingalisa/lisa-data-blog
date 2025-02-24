ee38277e60361b1768ecaf7a95272e726932c454
Lusingalisa <lusingalisa7@gmail.com>
2025-02-24 12:06:51 +0300

    two divisions for each chapter

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,29 @@
 export default function Home(){
 
-  let first_number=923;
-  let second_number=7;
-  let sum = first_number+second_number
-
-  let username="John Doe"
-  let username2="Maria Sky"
-  return( 
-  <div style={{
+  let ourStyles = {
     color:"black", 
     backgroundColor: "#5ef2cd", 
     border:"10px solid black", 
     padding: "10px", 
     margin:"50px",
-    borderRadius:"20px"}}>
-    <h1>DATA SCIENCE FOR AFRICA</h1>
-    <h2>An initiative for 2025</h2>
-    <p>Welcome to our website</p>
-    <strong>{10+10*1000}</strong>
-    <br/>
-    <em>{sum}</em>
-    <p>Greetings {username} </p>
-    <p>Greetings {username2} </p>
+    borderRadius:"20px"
+  }
+
+  return(
+    <div>
+    <div style={ourStyles}>
+      <h2>DATA SCIENCE</h2>
+      <h3>data the new oil</h3>
+      <p>Join our data science club to explore the fascinating world of data analysis, machine learning</p>
+
+    </div>
+    <div style={ourStyles}>
+    <h2>CYBER SECURITY</h2>
+    <h3>Guardians of the bytes</h3>
+    <p>Join our data science club to explore the fascinating world of data analysis, machine learning</p>
+
+  </div>
   </div>
   )
 }
+  
