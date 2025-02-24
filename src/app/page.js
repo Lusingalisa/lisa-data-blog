43008c9bcd40ce2028d6f503e96cef73a33e82b1
Lusingalisa <lusingalisa7@gmail.com>
2025-02-24 11:50:25 +0300

    padding

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,12 @@ export default function Home(){
   let username="John Doe"
   let username2="Maria Sky"
   return( 
-  <div style={{color:"red", backgroundColor: "#5ef2cd", border:"10px solid black"}}>
+  <div style={{
+    color:"red", 
+    backgroundColor: "#5ef2cd", 
+    border:"10px solid black", 
+    padding: "10px", 
+    margin:"50px"}}>
     <h1>DATA SCIENCE FOR AFRICA</h1>
     <h2>An initiative for 2025</h2>
     <p>Welcome to our website</p>
